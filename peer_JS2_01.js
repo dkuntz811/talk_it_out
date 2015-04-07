459eff635a0b65ae37dd9361050a941d50cb56e6
Scott Bromander <scott@primeacademy.io>
2015-04-07 12:07:38 -0500

    Orange commit

--- peer_JS2_01.js
+++ peer_JS2_01.js
@@ -1,11 +1,11 @@
 /*
-1. Start with the number 85 and set that equal to 'value'
+1. Start with the number 14 and set that equal to 'value'
 
 2. Create condition logic to check if the value is great or equal to 86
-2-1. If true, add 62 to 'value'
-2-2. If false, subtract 45 from 'value'
+2-1. If true, add 14 to 'value'
+2-2. If false, subtract 2 from 'value'
 
-3. Create a string that is set to 22, add it to 'value'
+3. Create a string that is set to 64, add it to 'value'
 
 4. Create an array, loop through 'value' using charAt, set array[i] to each value
 
@@ -19,10 +19,10 @@
 
 8. Add 'value' and the new variable created in Step 6 together and store them in 'value'
 
-9. If the new value of 'value' is greater than 4040, set 'value' equal to 2. If not, check to see if it is equal to 60, if it is, set 'value' equal to 98. 
-If neither of these are true, set the value to 1.
+9. If the new value of 'value' is greater than 1326, set 'value' equal to 2. If not, check to see if it is equal to 4040, if it is, set 'value' equal to 13. 
+If neither of these are true, set the value to 9.
 
-10. Create a while loop that counts down from 11 and increments 'value' by 1.
+10. Create a while loop that counts down from 9 and increments 'value' by 1.
 
 11. Create a function that accepts an argument 'val'. The function should convert 'val' to a String, then drop the first character off the String, but only if there 
 is more than 1 character in the String. Return 'val' and set the 'value' to the returned value.
@@ -31,5 +31,5 @@ is more than 1 character in the String. Return 'val' and set the 'value' to the 
 
 13. Console log value. Create an index.html file, link the this Javascript file. Load in the browser, check the value.
 
-14. Your answer should be a String value that equals 3. Is that what you got?
+14. Your answer should be a String value that equals 8. Is that what you got?
 */
