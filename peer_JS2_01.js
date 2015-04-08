1c4a1bf016f0ddba1f921a69230f89cc7378dde9
Scott Bromander <scott@primeacademy.io>
2015-04-07 20:07:58 -0500

    Green commit

--- peer_JS2_01.js
+++ peer_JS2_01.js
@@ -1,11 +1,11 @@
 /*
-1. Start with the number 32 and set that equal to 'value'
+1. Start with the number 74 and set that equal to 'value'
 
-2. Create condition logic to check if the value is great or equal to 23
-2-1. If true, add 3 to 'value'
-2-2. If false, subtract 1 from 'value'
+2. Create condition logic to check if the value is great or equal to 85
+2-1. If true, add 45 to 'value'
+2-2. If false, subtract 32 from 'value'
 
-3. Create a string that is set to 91, add it to 'value'
+3. Create a string that is set to 45, add it to 'value'
 
 4. Create an array, loop through 'value' using charAt, set array[i] to each value
 
@@ -18,11 +18,11 @@
 ******* SWITCH PROGRAMMERS *******
 
 8. Add 'value' and the new variable created in Step 6 together and store them in 'value'
+1
+9. If the new value of 'value' is greater than 5245, set 'value' equal to 9. If not, check to see if it is equal to 4287, if it is, set 'value' equal to 8. 
+If neither of these are true, set the value to 7.
 
-9. If the new value of 'value' is greater than 3287, set 'value' equal to 8. If not, check to see if it is equal to 3686, if it is, set 'value' equal to 13. 
-If neither of these are true, set the value to 1.
-
-10. Create a while loop that counts down from 4 and increments 'value' by 1.
+10. Create a while loop that counts down from 13 and increments 'value' by 1.
 
 11. Create a function that accepts an argument 'val'. The function should convert 'val' to a String, then drop the first character off the String, but only if there 
 is more than 1 character in the String. Return 'val' and set the 'value' to the returned value.
@@ -31,5 +31,5 @@ is more than 1 character in the String. Return 'val' and set the 'value' to the 
 
 13. Console log value. Create an index.html file, link the this Javascript file. Load in the browser, check the value.
 
-14. Your answer should be a String value that equals 2. Is that what you got?
+14. Your answer should be a String value that equals 1. Is that what you got?
 */
