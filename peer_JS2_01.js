79ff2cac51f679aa6b8fceb24e1c6115680a6f5b
Scott Bromander <scott@primeacademy.io>
2015-04-07 20:43:56 -0500

    Gray commit

--- peer_JS2_01.js
+++ peer_JS2_01.js
@@ -1,11 +1,11 @@
 /*
-1. Start with the number 35 and set that equal to 'value'
+1. Start with the number 68 and set that equal to 'value'
 
-2. Create condition logic to check if the value is great or equal to 58
-2-1. If true, add 7 to 'value'
-2-2. If false, subtract 2 from 'value'
+2. Create condition logic to check if the value is great or equal to 81
+2-1. If true, add 9 to 'value'
+2-2. If false, subtract 1 from 'value'
 
-3. Create a string that is set to 78, add it to 'value'
+3. Create a string that is set to 95, add it to 'value'
 
 4. Create an array, loop through 'value' using charAt, set array[i] to each value
 
@@ -19,10 +19,10 @@
 
 8. Add 'value' and the new variable created in Step 6 together and store them in 'value'
 
-9. If the new value of 'value' is greater than 3451, set 'value' equal to 24. If not, check to see if it is equal to 7923, if it is, set 'value' equal to 67. 
-If neither of these are true, set the value to 19.
+9. If the new value of 'value' is greater than 3271, set 'value' equal to 59. If not, check to see if it is equal to 1069, if it is, set 'value' equal to 95. 
+If neither of these are true, set the value to 11.
 
-10. Create a while loop that counts down from 17 and increments 'value' by 1.
+10. Create a while loop that counts down from 6 and increments 'value' by 1.
 
 11. Create a function that accepts an argument 'val'. The function should convert 'val' to a String, then drop the first character off the String, but only if there 
 is more than 1 character in the String. Return 'val' and set the 'value' to the returned value.
@@ -31,5 +31,5 @@ is more than 1 character in the String. Return 'val' and set the 'value' to the 
 
 13. Console log value. Create an index.html file, link the this Javascript file. Load in the browser, check the value.
 
-14. Your answer should be a String value that equals 6. Is that what you got?
+14. Your answer should be a String value that equals 5. Is that what you got?
 */
