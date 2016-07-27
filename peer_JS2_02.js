eb06728aca36f9b8f9c1c93b1dfeb946308b529b
Spengs <Jotto465@gmail.com>
2016-07-27 14:56:08 -0500

    first commit

---
+++ peer_JS2_02.js
@@ -0,0 +1,33 @@
+var value = 42 
+
+if(value >= 53){
+    value + 42;
+} else {
+    value - 13;
+}
+
+var value = "11" + value
+
+var array = []
+
+for(i = 0; i < value.length; i++) {
+	    array [i] = value.charAt(i);
+}
+
+array.pop();
+array.shift();
+console.log(array);
+
+var joe = "";
+
+for(i = array.length - 1; i >= 0; i--){
+	   joe = joe + array[i];
+      
+}
+
+parseInt(value);
+parseInt(joe);
+typeof(value);
+typeof(joe);
+console.log(value);
+//Partnered with Dave
